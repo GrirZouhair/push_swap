b14875f107a9761e02136519414adee3a1d41052
Zouhair Grir <zogrir@e2r9p10.1337.ma>
2025-01-16 17:53:16 +0100

    create valid_num checker

---
+++ push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zogrir <zogrir@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/13 12:11:39 by zogrir            #+#    #+#             */
+/*   Updated: 2025/01/16 17:48:25 by zogrir           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+#include<stdlib.h>
+char		**ft_split(char const *s, char c);
+void		ft_free(char **arr);
+int			ft_valid_num_check(const char *str);
+
+
+#endif
---
+++ valid_num_check.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   valid_num_check.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zogrir <zogrir@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/16 15:07:18 by zogrir            #+#    #+#             */
+/*   Updated: 2025/01/16 17:52:48 by zogrir           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"push_swap.h"
+
+int	ft_valid_num_check(const char *str)
+{
+	char	**splited;
+	int		i;
+	int		j;
+
+	i = 0;
+	splited = ft_split(str, ' ');
+	
+	if (!splited || !splited[0])
+		return (ft_free(splited), 0);
+	while (splited[i])
+	{
+		j = 0;
+		if(splited[i][0] == '+' || splited[i][0] == '-')
+		{
+			if (splited[i][1] == '+' || splited[i][1] == '-')
+				return (ft_free(splited), 0);
+		}
+		while (splited[i][j])
+		{
+			if (!(splited[i][j] >= '0' && splited[i][j] <= '9'))
+			{
+				return (ft_free(splited), 0);
+			}
+			j++;
+		}
+		i++;
+	}
+	
+	return (ft_free(splited), 1);
+}
